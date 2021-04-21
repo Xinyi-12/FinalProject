beae3a7696119d69aafdd7b4eb984837726cecdf
Xinyi-12 <71161607+Xinyi-12@users.noreply.github.com>
2021-04-21 22:38:29 +0800

    Update Constants.java

--- finalVirusProject/VirusBroadcastController/src/Constants.java
+++ finalVirusProject/VirusBroadcastController/src/Constants.java
@@ -4,20 +4,20 @@ public class Constants {
     public static final int CITY_WIDTH = 840;
     public static final int CITY_HEIGHT = 760;
 
-    public int originalCount = 50;//初始感染数量
-    public static float broadRate = 0.8f;//传播率
-    public float shadowTime = 0;//潜伏时间，14天为140
-    public int hospitalReceiveTime = 10;//医院收治响应时间
-    public int bedCount = 0;//医院床位
+    public int originalCount = 50;//Initial number of infections
+    public static float broadRate = 0.8f;//Dissemination rate
+    public float shadowTime = 0;//Incubation time, 14 days for 140
+    public int hospitalReceiveTime = 10;//Hospital admission response time
+    public int bedCount = 0;//Hospital Beds
 
     public float u = 0.99f;
-    public int cityPersonSize = 5000;//城市总人口数量
-    public float fatalityRate = 0.50f;//fatality_rate病死率，根据2月6日数据估算（病死数/确诊数）为0.02
-    public int dieTime = 100;//死亡时间均值，30天，从发病（确诊）时开始计时
-    public double dieVariance = 1;//死亡时间方差
+    public int cityPersonSize = 5000;//Total city population
+    public float fatalityRate = 0.50f;//fatality_rate
+    public int dieTime = 100;//Mean time to death, 30 days, from onset (diagnosis)
+    public double dieVariance = 1;//Time variance of death
 
 
-    public float fatalityRateHospital = 0.25f;//fatality_rate病死率，根据2月6日数据估算（病死数/确诊数）为0.02
+    public float fatalityRateHospital = 0.25f;//fatality_rate
     public int cureTime = 100;
     public double cureVariance = 1;
 
